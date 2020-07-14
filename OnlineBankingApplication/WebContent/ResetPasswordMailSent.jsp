93f44d71a25ac9a78b50f34fac982a69a24cfd4c
Vini Mehta <vini.mehta78@gmail.com>
2020-07-14 13:51:51 +0530

    forgot password added

---
+++ OnlineBankingApplication/WebContent/ResetPasswordMailSent.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <link href="assets/css/bootstrap-theme.min.css" rel="stylesheet" />
+
+    <title>Insert title here</title>
+  </head>
+
+  <body>
+    <!-- banner -->
+    <section>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h1 class="display-2 text-capitalize">
+              Reset password link sent successfully to your registered E-mail Id.
+            </h1>
+          </div>
+        </div>
+
+        <center>
+          <form action="IndexPage.jsp">
+            <button type="submit">OK</button>
+          </form>
+        </center>
+      </div>
+    </section>
+    <!-- end of banner -->
+  </body>
+</html>
---
+++ OnlineBankingApplication/src/com/Controller/ForgotPasswordController.java
@@ -0,0 +1,92 @@
+package com.Controller;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ForgotPasswordController
+ */
+@WebServlet("/ForgotPasswordController")
+public class ForgotPasswordController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ForgotPasswordController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String receiverAddress = request.getParameter("emailId");
+		HttpSession httpSession=request.getSession(true);
+	    httpSession.setAttribute("toMail",receiverAddress);
+	    
+	    String subject="Bank account password reset link"; 
+		String bodyMessage = "Dear customer,\n"
+				+ "We heard that you lost your Bank account password. Sorry about that!\n"
+				+ "But don’t worry! You can now reset your password by clicking the below link or copying and pasting it into your browser:\n\n"
+				+ "http://localhost:8087/OnlineBankingApplication/ResetPassword.jsp\n\n"
+				+ "If you didn't request this, please ignore this email. If you experience any difficulty, please visit our website.\n"
+				+ "http://localhost:8087/OnlineBankingApplication/HomePage.jsp\n"
+				+ "Your password won't change until you access the link above and create a new one.\n\n"
+				+ "Thank you,\n"
+				+ "Bank";
+		final String username = "vini.mehta78@gmail.com";
+		final String password = "qgtvqkizrzaurqwp";
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "587");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.starttls.enable", "true");
+
+		Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverAddress));
+			message.setSubject(subject);
+			message.setText(bodyMessage);
+
+			Transport.send(message);
+
+			System.out.println("Done");
+		    response.sendRedirect("/OnlineBankingApplication/ResetPasswordMailSent.jsp");
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
